5572e62c08648d593de820473698013e38b25c04
1aperez <42749606+1aperez@users.noreply.github.com>
2019-01-17 13:56:53 -0600

    Add files via upload

--- player.js
+++ player.js
@@ -21,11 +21,58 @@ Def:20,
 Sp: 20
 }
 
-let battle = {
+let p1Battle = {
 
-  attack: function(){},
-  defend: function(){}
+  attack: function(mv){
+    if(mv==1 ){
+      return this.moves.punch();
 
+    }
+    else if(mc==2){
+      this.moves.other
+  }
+  
+  },
+  defend: function(atkmv){
+    let rawDamage =atkmv - this.states.def
+    this.stats.hp=this.stats-rawDamage;
+    if(this.stats.hp<0){
+      this.stats.hp=0;
+    }
+  }
+
+}
+let physical = {
+  razorPunch: function(){
+    let baseDamage= this.stat.atk*0.75
+    let bonus = 0.5*this.stats.atk*Math.random();
+    return baseDamage+bonus
+  },
+  charge: function(){
+    let pain= Math.random();
+    if(pain<0.50){
+      this.stats.hp= this.sats.hp-this.stats.atk/4;
+      return this.stats.atk+bonus
+    }
+  }
+}
+let magic={
+  hydroPump: function(){
+    if(this.stats.mp>=8){
+      this.stats.mp= this.stats.mp-8;
+      let tetra=Math.random();
+      let pump=Math.ceil(this.stats.sp/3)
+       if(tetra<0.10){
+        return 2*pump;
+      }
+      else if(tetra<0.10){
+        return 3*pump;
+      }
+      else if(tetra<0.10){
+        return 4*pump;
+      }
+    }
+  }
 }
 
 
